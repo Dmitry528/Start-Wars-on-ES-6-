ea576a55be668ee2f2f5bb9348633ee5a4357859
Dmitry Chekurkov <chekurkov@gmail.com>
2019-05-10 14:21:14 +0300

    get basic api-request

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";var xhr=new XMLHttpRequest;xhr.open("GET","https://swapi.co/api/people/",!0),xhr.send(),xhr.onreadystatechange=function(){if(4==xhr.readyState)if(200!=xhr.status){var e=xhr.status,t=xhr.statusText;console.log(e+": "+t)}else{var s=JSON.parse(xhr.responseText);console.log(s)}};
 //# sourceMappingURL=main.js.map
